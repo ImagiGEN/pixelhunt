8f80de5e94d4baf6b25b79fa57be11648fb77de8
Ashritha Goramane <ashritha.goramane@gmail.com>
2023-07-08 15:10:16 -0400

    Added Skeleton code

---
+++ streamlit/Home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from dotenv import load_dotenv
+import google.cloud.storage as gcs
+
+load_dotenv()
+
+st.set_page_config(
+    page_title="Home Page",
+    page_icon="👋",
+)
+
+st.title("Image Processing with Azure CV")
+
+st.markdown(
+    """
+    Application for searching through apparels image database using image and prompt search.
+"""
+)
+
+# Run the app
+# streamlit run main.py
